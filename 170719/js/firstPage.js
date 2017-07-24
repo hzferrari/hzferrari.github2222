6500e0bda3b3b9b74082cef4e7c8dcd6a9dda943
hzferrari <hzferrari@qq.com>
2017-07-24 10:15:21 +0800

    1

--- 170719/js/firstPage.js
+++ 170719/js/firstPage.js
@@ -69,8 +69,8 @@
 
 	//“去和彩云使用”按钮效果
 	$("#openAndDown").click(function(){
-	   $("#openAndDown").css("cssText","width:59% !important");
-	   setTimeout(function(){$("#openAndDown").css("cssText","width:60% !important");},200);
+	   $("#openAndDown").css("cssText","width:58% !important");
+	   setTimeout(function(){$("#openAndDown").css("cssText","width:60% !important");},400);
 	});
 	//活动规则弹窗
 	$(".Windowpop2").click(function () {
