68f639d9fe2240ce1e0f3b47641f15268a1382b8
hzferrari <hzferrari@qq.com>
2017-07-25 13:07:34 +0800

    2

--- 170719/js/firstPage.js
+++ 170719/js/firstPage.js
@@ -6,21 +6,21 @@
 	// 	var $this = this;
 	// 	viewScroll($this);
 	// })
-	$("#phoneNum，#identifyCode").on("click",function(){
-		if(phClickFlag === false){
-			var $this = this;
-			viewScroll($this);
-			phClickFlag === true;
-		}	
-	})
-	var viewScroll = function(Bt){
-		var $this = Bt;
-		$(".rule").css("margin-bottom","300px");
-		$this.scrollIntoView();
-		setTimeout(function(){
-			$(".rule").css("margin-bottom","40px");
-		},20);
-	}
+	// $("#phoneNum，#identifyCode").on("click",function(){
+	// 	if(phClickFlag === false){
+	// 		var $this = this;
+	// 		viewScroll($this);
+	// 		phClickFlag === true;
+	// 	}	
+	// })
+	// var viewScroll = function(Bt){
+	// 	var $this = Bt;
+	// 	$(".rule").css("margin-bottom","300px");
+	// 	$this.scrollIntoView();
+	// 	setTimeout(function(){
+	// 		$(".rule").css("margin-bottom","40px");
+	// 	},20);
+	// }
 	//点击“获取验证码”按钮，检测输入是否符合规则，切是否属于白名单
 	document.getElementById("getCode").onclick = function(){
 		checkWhithList(this);
