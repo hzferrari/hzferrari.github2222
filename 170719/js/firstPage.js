142b85b8e71c7ba4faec19b960f81f865e7eff29
hzferrari <hzferrari@qq.com>
2017-07-25 11:54:34 +0800

    1

--- 170719/js/firstPage.js
+++ 170719/js/firstPage.js
@@ -2,11 +2,11 @@
 	//点击输入框时，页面滚动到该位置
 	$("#phoneNum,#identifyCode").on("click",function(){
 		$this = this;
-		$(".rule").css("margin-bottom","300px");
+		$(".rule").css("margin-bottom","280px");
 		$this.scrollIntoView();
 		setTimeout(function(){
 			$(".rule").css("margin-bottom","40px");
-		},100);
+		},20);
 		
 	})
 	//点击“获取验证码”按钮，检测输入是否符合规则，切是否属于白名单
